371956a45053c89a98696153aebbb0d0fe4eca2b
aswordlessninja <aswordlessninja@gmail.com>
2018-01-26 15:54:58 -0700

    Fixed it

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
 class Main{
-    // Cucks for days
+    // I had this method in the wrong place. Fixed it
+    public static void foo(){
+        System.out.println("Oh boi, here I come!");
+    }
+    public static void main(String[] args){// noooo!
+        foo();
+    }
+    // Yeah I have no clue what I'm doing
 }
