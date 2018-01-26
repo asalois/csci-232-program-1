f2af1aad19d9b0a3f6f1f97b506b61a1eb4d7e68
asalo <corvettelover1997@gmail.com>
2018-01-26 15:35:51 -0700

    nooo!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 class Main{
     // Cucks for days
-    public static void main(String[] args){
+    public static void main(String[] args){// noooo!
         public void foo(){
             System.out.println("Oh boi, here I come!");
         }
