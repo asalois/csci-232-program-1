724a533d39df3e09c0d0ceee6f3c154dd299b5a7
aswordlessninja <aswordlessninja@gmail.com>
2018-01-26 15:37:48 -0700

    Fixed that shit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 class Main{
     // Cucks for days
-    public static void main(String[] args){// noooo!
-        public void foo(){
-            System.out.println("Oh boi, here I come!");
-        }
+
+    public static void foo(){
+        System.out.println("Oh boi, here I come!");
     }
+    public static void main(String[] args){// noooo!
+        foo();
+    }
+
 }
