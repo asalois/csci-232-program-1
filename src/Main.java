79b2cc212546c23f283ec4d1987fbff262016933
aswordlessninja <aswordlessninja@gmail.com>
2018-01-26 15:32:01 -0700

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
 class Main{
     // Cucks for days
+    public static void main(String[] args){
+        public void foo(){
+            System.out.println("Oh boi, here I come!");
+        }
+    }
 }
